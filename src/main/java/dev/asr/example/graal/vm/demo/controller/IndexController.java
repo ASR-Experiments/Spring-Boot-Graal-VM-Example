962da69a0517252f8e44f653049340a9e5558046
Arvind Singh Rawat <arvind160102261@gmail.com>
2025-12-05 19:46:49 +0530

    #2: Added logic to read from Yaml/Properties file
    Signed-off-by: Arvind Singh Rawat <arvind160102261@gmail.com>

--- src/main/java/dev/asr/example/graal/vm/demo/controller/IndexController.java
+++ src/main/java/dev/asr/example/graal/vm/demo/controller/IndexController.java
@@ -1,6 +1,7 @@
 package dev.asr.example.graal.vm.demo.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("")
 public class IndexController {
 
+    @Value("${message.hello-world:Hello World !!!}")
+    String helloWorldMessage;
+
     @GetMapping
     public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok("Hello World !!!");
+        return ResponseEntity.ok(helloWorldMessage);
     }
 }
