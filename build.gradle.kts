8c8df9b692d811c29554b74489b0e4f011724302
Arvind Singh Rawat <arvind160102261@gmail.com>
2025-12-13 16:34:43 +0530

    #4: Added config to build native image from Gradle
    Signed-off-by: Arvind Singh Rawat <arvind160102261@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
 plugins {
-	java
-	id("org.springframework.boot") version "4.0.0"
-	id("io.spring.dependency-management") version "1.1.7"
-	id("org.graalvm.buildtools.native") version "0.11.3"
+    java
+    id("org.springframework.boot") version "4.0.0"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.graalvm.buildtools.native") version "0.11.3"
 }
 
 group = "dev.asr.example"
@@ -10,21 +12,38 @@ version = "0.0.1-SNAPSHOT"
 description = "Demo project for Spring Boot with Graal VM"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(25)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+tasks.named<BootBuildImage>("bootBuildImage") {
+
+    // Custom image name and tag
+    imageName.set("graal-vm-demo:0.0.3")
+
+    // Specify a different builder
+    builder.set("paketobuildpacks/builder-jammy-buildpackless-tiny")
+
+    // Manually specify buildpacks
+    buildpacks.set(
+        listOf(
+            "paketobuildpacks/oracle",
+            "paketobuildpacks/java-native-image"
+        )
+    )
 }
